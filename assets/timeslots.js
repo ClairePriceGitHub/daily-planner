768309eaa3a999a6e92738a81eb2fb25ec4f5e82
Claire Price <claire.price@mail.com>
2024-01-05 23:09:22 +0000

    add hour numbers in 24-hour format

--- assets/timeslots.js
+++ assets/timeslots.js
@@ -1,5 +1,49 @@
 var timeSlots = [
     {
-        
+        slotNumber: 0,
+        name: '9am',
+        hour: 9,
+    },
+    {
+        slotNumber: 1,
+        name: '10am',
+        hour: 10,
+    },
+    {
+        slotNumber: 1,
+        name: '11am',
+        hour: 11,
+    },
+    {
+        slotNumber: 1,
+        name: '12pm',
+        hour: 12,
+    },
+    {
+        slotNumber: 1,
+        name: '1pm',
+        hour: 13,
+    },
+    {
+        slotNumber: 1,
+        name: '2pm',
+        hour: 14,
+    },
+    {
+        slotNumber: 1,
+        name: '3pm',
+        hour: 15,
+    },
+    {
+        slotNumber: 1,
+        name: '4pm',
+        hour: 16,
+    },
+    {
+        slotNumber: 1,
+        name: '5pm',
+        hour: 17,
     }
 ]
+
+
