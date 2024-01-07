d938519d8614c520db5dff8a80e093e543de545f
Claire Price <claire.price@mail.com>
2024-01-07 01:42:24 +0000

    add input fields to array of objects

--- assets/timeslots.js
+++ assets/timeslots.js
@@ -2,47 +2,56 @@ var timeSlots = [
     {
         slotNumber: 0,
         name: '9am',
-        hour: 9,
+        hour: 0,
+        input: ''
     },
     {
         slotNumber: 1,
         name: '10am',
-        hour: 10,
+        hour: 1,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 2,
         name: '11am',
         hour: 11,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 3,
         name: '12pm',
         hour: 12,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 4,
         name: '1pm',
         hour: 13,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 5,
         name: '2pm',
         hour: 14,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 6,
         name: '3pm',
         hour: 15,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 7,
         name: '4pm',
         hour: 16,
+        input: ''
     },
     {
-        slotNumber: 1,
+        slotNumber: 8,
         name: '5pm',
         hour: 17,
+        input: ''
     }
 ]
 
