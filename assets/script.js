420be2ccf6a1a4994fff86f7e7f9c3b21429dd52
Claire Price <claire.price@mail.com>
2024-01-08 21:01:56 +0000

    update variable names

--- assets/script.js
+++ assets/script.js
@@ -12,55 +12,55 @@ $( document ).ready(function() {
 
 
 
-    
+
     // FUNCTION to show todays date in header
     function dateToday() {
-        var today = dayjs().format('dddd, MMMM D') + ('th');
-        $('#currentDay').text(today);
+        var $today = dayjs().format('dddd, MMMM D') + ('th');
+        $('#currentDay').text($today);
     };
     dateToday();
 
     // Dynamically add p tag to existing container div, confirming save to local storage // Set to hide
-    var confirm = $('<p class="time-block">').text('event added to local storage');
-    $('.container').append(confirm);
-    confirm.hide();
+    var $confirm = $('<p class="time-block">').text('event added to local storage');
+    $('.container').append($confirm);
+    $confirm.hide();
 
     // Dynamically add Form tag and other contents tags to existing container div 
     timeSlots.forEach(function(timeSlot) {
         // Add class to form tag and append to container div 
-        var row = $('<form class="row">');
-        $('.container').append(row);
+        var $row = $('<form class="row">');
+        $('.container').append($row);
         // Add attributes to label tag, add text and styling
-        var hour = $('<label for="input" class="hour col-1">').text(timeSlot.name);
-        hour.css({
+        var $hour = $('<label for="input" class="hour col-1">').text(timeSlot.name);
+        $hour.css({
             display: 'flex',
             textAlign: 'center',
             alignItems: 'center'
         });
         // Add attributes to textarea tag // Check past/present/future and assign class accordingly
-        var currentHour = dayjs().format('HH');
+        var $currentHour = dayjs().format('HH');
         var plannerHour = timeSlot.hour;
-        var eventInput = $('<textarea id="input" name="event-text" rows="3">').attr('index', timeSlot.slotNumber);
-        if (plannerHour < currentHour) {
-            eventInput.addClass('past col-10');
-        } else if (plannerHour == currentHour) {
-            eventInput.addClass('present col-10');
+        var $eventInput = $('<textarea id="input" name="event-text" rows="3">').attr('index', timeSlot.slotNumber);
+        if (plannerHour < $currentHour) {
+            $eventInput.addClass('past col-10');
+        } else if (plannerHour == $currentHour) {
+            $eventInput.addClass('present col-10');
         } else {
-            eventInput.addClass('future col-10');
+            $eventInput.addClass('future col-10');
         };
       
 
-        var submitButton = $('<button class="saveBtn col-1" >').attr('index', timeSlot.slotNumber);
-        submitButton.css({
+        var $submitButton = $('<button class="saveBtn col-1" >').attr('index', timeSlot.slotNumber);
+        $submitButton.css({
             borderRight: 'none',
             borderTop: 'none',
             borderBottom: 'none'
         });
         // Append to form tag
-        row.append(hour, eventInput, submitButton);
+        $row.append($hour, $eventInput, $submitButton);
 
-        var submitBtnImg = $('<img src="images/1_floppy-disk.png" width="30px" height="30px">');
-        submitButton.append(submitBtnImg);
+        var $submitBtnImg = $('<img src="images/1_floppy-disk.png" width="30px" height="30px">');
+        $submitButton.append($submitBtnImg);
     });
 
     // FUNCTION to save to local storage
@@ -70,18 +70,18 @@ $( document ).ready(function() {
 
     // FUNCTION to get data from local storage and add to textarea tag values
     function updateDisplay() {
-        var getTimeSlots = JSON.parse(localStorage.getItem('userInputs'));
+        var $getTimeSlots = JSON.parse(localStorage.getItem('userInputs'));
         $('.row #input').each(function(index) {
-            $(this).text(getTimeSlots[index].input);
+            $(this).text($getTimeSlots[index].input);
         });  
     };
     updateDisplay();
 
     // FUNCTION to confirm save to local storage 
     function saveConfirm() {
-        confirm.show();
+        $confirm.show();
         setInterval(function() {
-            confirm.hide();
+            $confirm.hide();
             }, 2000);
     };
     
